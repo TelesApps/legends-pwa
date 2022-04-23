23188dde90886fb70ca40be9366d0d5a4010d9aa
Claudio Teles <telesapps85@gmail.com>
2022-04-23 07:17:12 -0400

    Character stats interface

--- src/app/interfaces/character.interface.ts
+++ src/app/interfaces/character.interface.ts
@@ -2,6 +2,7 @@ export interface Character {
     playerId: string;
     characterId: string;
     primaryStats: PrimaryStats
+    equipmentModifier: Array<StatusEffect>;
 }
 
 interface PrimaryStats {
@@ -12,11 +13,43 @@ interface PrimaryStats {
     stamina: number;
     maxPower: number;
     power: number;
-    core_melee: number;
-    weaponAttack: number;
-    meleeWeaponSkillModifier: number;
-    meleeAttack: number;
     core_ranged: number;
     rangedAttack: number;
+    core_melee: number;
+    meleeAttack: number;
     core_defense: number;
+    defense: number;
+    core_strength: number;
+    strength: number;
+    core_agility: number;
+    agility: number;
+    core_accuracy: number;
+    accuracy: number;
+    core_perception: number;
+    perception: number;
+    core_mental: number;
+    mental: number;
+    core_movement: number;
+    movement: number;
+}
+
+interface SecondaryStats {
+    core_stealth: number;
+    stealth: number;
+    core_lockPicking: number;
+    lockPicking: number;
+    core_quickHands: number;
+    quickHands: number;
+    core_persuasion: number;
+    persuasion: number;
+    core_intimidation: number;
+    intimidation: number;
+}
+
+interface StatusEffect {
+    id?: number;
+    stat: string;
+    value: number;
+    duration?: number;
+    durationCount?: number;
 }
