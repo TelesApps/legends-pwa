05157957a97f9bf8d0956eef2b9ed1bef2de1ea5
Claudio Teles <telesapps85@gmail.com>
2022-04-30 09:45:43 -0400

    calculating other start with a subscription

--- src/app/services/character-creation.service.ts
+++ src/app/services/character-creation.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ReplaySubject } from 'rxjs';
+import { ReplaySubject, Subscription } from 'rxjs';
 import { Character, CreateNewCharacter } from '../interfaces/character.interface';
 
 @Injectable({
@@ -12,15 +12,25 @@ export class CharacterCreationService {
   abilityPoints = 30;
   skillsPoints = 30;
   characterSubj: ReplaySubject<Character> = new ReplaySubject<Character>(1);
+  statSubscription: Subscription
   constructor() {
     const character = CreateNewCharacter();
     this.characterSubj.next(character);
+    this.setCharacterStats();
+  }
+
+  setCharacterStats() {
+    // Set character's calculated stat based on user selection
+    this.statSubscription = this.characterSubj.subscribe((character) => {
+      character.primaryStats.maxHealth = character.primaryStats.maxStamina + (character.primaryStats.core_strength * 10) / 2;
+    })
   }
 
   saveNewCharacterToCloud() {
     // Perform logic and function to save character to Firestore
     // Unsubscribe and delete data.
     this.characterSubj.unsubscribe();
+    this.statSubscription.unsubscribe();
   }
 
 }
