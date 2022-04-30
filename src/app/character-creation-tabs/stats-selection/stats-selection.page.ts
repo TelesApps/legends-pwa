ac009e6a9a642c83d64f98e849d61745f85fae6a
Claudio Teles <telesapps85@gmail.com>
2022-04-30 09:27:18 -0400

    creation all stats alocation working

--- src/app/character-creation-tabs/stats-selection/stats-selection.page.ts
+++ src/app/character-creation-tabs/stats-selection/stats-selection.page.ts
@@ -18,14 +18,17 @@ export class StatsSelectionPage implements OnInit {
   }
 
   onStatChange(stat: string, value: number) {
-    if(this.creation.statsPoints > 0) {
+    if(this.creation.statsPoints > 0 || value < 0) {
       value > 0 ? this.creation.statsPoints-- : this.creation.statsPoints++;
       switch (stat) {
         case 'H': this.character.primaryStats.maxHealth += (value * 3); break;
         case 'Stam': this.character.primaryStats.maxStamina += (value * 5); break;
         case 'P': this.character.primaryStats.maxStamina += (value * 5); break;
-        case 'RA': this.character.primaryStats.core_ranged += value; break;
-        case 'MA': this.character.primaryStats.core_melee += value; break;
+        case 'Str': this.character.primaryStats.core_strength += value; break;
+        case 'Ag': this.character.primaryStats.core_agility += value; break;
+        case 'Acc': this.character.primaryStats.core_accuracy += value; break;
+        case 'Per': this.character.primaryStats.core_perception += value; break;
+        case 'M': this.character.primaryStats.core_mental += value; break;
       }
       this.creation.characterSubj.next(this.character);
     }
