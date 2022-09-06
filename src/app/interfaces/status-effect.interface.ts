c63a567c46a3e2c04e7de5398332add639077916
Claudio Teles <telesapps85@gmail.com>
2022-09-06 06:35:56 -0400

    new character status added

--- src/app/interfaces/status-effect.interface.ts
+++ src/app/interfaces/status-effect.interface.ts
@@ -7,7 +7,8 @@ export interface StatusEffect {
 }
 
 export enum Stat {
-    MaxHealth, Health, MaxStamina, Stamina, MaxPower, Power, RangeAttack, MeleeAttack, MeleeDmgDelt, RangeDmdDelt, Defence, Strength, Agility, Accuracy, Perception, Mental, Movement,
-    Crit, MaxArmor, Armor, DamageResistance, Counter,
-    stealth, lockPicking, quickHands, persuasion, intimidation, engineering, tracking, scoutScavange, mining, herbalism, alchemy, carryCapacity
+    MaxHealth, Health, MaxStamina, Stamina, MaxPower, Power, RangeAttack, MeleeAttack, MeleeDmgDelt, RangeDmdDelt, Defence, 
+    Strength, Agility, Accuracy, Perception, Mental, Movement, Crit, MaxArmor, Armor, DamageResistance, Counter,
+    stealth, lockPicking, quickHands, persuasion, intimidation, courage, engineering, tracking, scoutScavange, mining, smithing, herbalism, 
+    alchemy, carryCapacity, phiralMining, phiralSmith
 }
