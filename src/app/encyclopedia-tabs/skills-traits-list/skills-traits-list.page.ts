defd51248aa1e2115d76ecb01de7f03ebd5a5cbe
Claudio Teles <telesapps85@gmail.com>
2022-10-23 06:49:51 -0400

    Skills prerequisites working

--- src/app/encyclopedia-tabs/skills-traits-list/skills-traits-list.page.ts
+++ src/app/encyclopedia-tabs/skills-traits-list/skills-traits-list.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { IonAccordionGroup } from '@ionic/angular';
+import { IonAccordionGroup, IonContent } from '@ionic/angular';
 import { SkillTraits } from 'src/app/interfaces/skills-traits.interface';
 import { AirtableDataService } from 'src/app/services/airtable-data.service';
 import { CharacterCreationService } from 'src/app/services/character-creation.service';
@@ -20,6 +20,7 @@ export class SkillsTraitsListPage implements OnInit {
   isSelectMode: boolean = false;
   backUrl: string = '/character-creation-tabs/skills-selection'
   backQuerySegmentSelection: string;
+  @ViewChild(IonContent, { static: false }) content: IonContent;
   @ViewChild('accordionGroup', { static: true }) accordionGroup: IonAccordionGroup;
   constructor(
     public airtable: AirtableDataService,
@@ -100,7 +101,7 @@ export class SkillsTraitsListPage implements OnInit {
 
   onSkillSelected(skill: SkillTraits) {
     this.creation.skillTraitsSelection = skill;
-    this.router.navigate([this.backUrl], {queryParams: {selection: this.backQuerySegmentSelection}});
+    this.router.navigate([this.backUrl], { queryParams: { selection: this.backQuerySegmentSelection } });
     console.log('Skill&Traits selected: ', skill)
     this.accordionGroup.value = undefined;
   }
@@ -123,9 +124,31 @@ export class SkillsTraitsListPage implements OnInit {
   hasSkill(title: string) {
     let selected;
     selected = this.creation.characterSelectedSkills.find(t => t.title === title);
-    if(selected) return selected;
+    if (selected) return selected;
     selected = this.creation.characterSelectedTraits.find(t => t.title === title);
     return selected
+  }
+
+  onGoToPrereq(prereq: string) {
+    if (this.searchInput || this.skillTagFilter && this.skillTagFilter.length > 0) {
+      this.searchInput = '';
+      this.skillTagFilter = [];
+      // this.onFilterChange();
+      setTimeout(() => {
+        this.onGoToPrereq(prereq);
+      }, 100);
+    } else {
+      if (this.accordionGroup.value === prereq) {
+        this.accordionGroup.value = undefined;
+      } else {
+        this.accordionGroup.value = prereq;
+        var scrollToElement = document.getElementById(prereq);
+        if (scrollToElement)
+          this.content.scrollToPoint(0, scrollToElement.offsetTop - 120, 1000);
+      }
+
+    }
+
   }
 
 }
